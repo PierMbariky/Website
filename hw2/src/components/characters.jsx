b954b209330e89e77bb1fdb8f1aaeefaf17950bb
PierMbariky <pier.mbariky@e.braude.ac.il>
2024-08-28 13:14:56 +0300

    ssasa

--- hw2/src/components/characters.jsx
+++ hw2/src/components/characters.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 // Array containing letters and their associated sound file paths
 const letters = [
-  { letter: 'ד', sound: '/sound/ד.mp3' },
-  { letter: 'ג', sound: '/sound/ג.mp3' },
-  { letter: 'ב', sound: '/sound/ב.mp3' },
   { letter: 'א', sound: '/sound/א.mp3' },
-  { letter: 'ח', sound: '/sound/ח.mp3' },
-  { letter: 'ז', sound: '/sound/ז.mp3' },
-  { letter: 'ו', sound: '/sound/ו.mp3' },
+  { letter: 'ב', sound: '/sound/ב.mp3' },
+  { letter: 'ג', sound: '/sound/ג.mp3' },
+  { letter: 'ד', sound: '/sound/ד.mp3' },
   { letter: 'ה', sound: '/sound/ה.mp3' },
-  { letter: 'ל', sound: '/sound/ל.mp3' },
-  { letter: 'כ', sound: '/sound/כ.mp3' },
-  { letter: 'י', sound: '/sound/י.mp3' },
+  { letter: 'ו', sound: '/sound/ו.mp3' },
+  { letter: 'ז', sound: '/sound/ז.mp3' },
+  { letter: 'ח', sound: '/sound/ח.mp3' },
   { letter: 'ט', sound: '/sound/ט.mp3' },
-  { letter: 'ע', sound: '/sound/ע.mp3' },
-  { letter: 'ס', sound: '/sound/ס.mp3' },
-  { letter: 'נ', sound: '/sound/נ.mp3' },
+  { letter: 'י', sound: '/sound/י.mp3' },
+  { letter: 'כ', sound: '/sound/כ.mp3' },
+  { letter: 'ל', sound: '/sound/ל.mp3' },
   { letter: 'מ', sound: '/sound/מ.mp3' },
-  { letter: 'ר', sound: '/sound/ר.mp3' },
-  { letter: 'ק', sound: '/sound/ק.mp3' },
-  { letter: 'צ', sound: '/sound/צ.mp3' },
+  { letter: 'נ', sound: '/sound/נ.mp3' },
+  { letter: 'ס', sound: '/sound/ס.mp3' },
+  { letter: 'ע', sound: '/sound/ע.mp3' },
   { letter: 'פ', sound: '/sound/פ.mp3' },
-  { letter: 'ת', sound: '/sound/ת.mp3' },
-  { letter: 'ש', sound: '/sound/ש.mp3' }
-  // Add more letters and sound paths
+  { letter: 'צ', sound: '/sound/צ.mp3' },
+  { letter: 'ק', sound: '/sound/ק.mp3' },
+  { letter: 'ר', sound: '/sound/ר.mp3' },
+  { letter: 'ש', sound: '/sound/ש.mp3' },
+  { letter: 'ת', sound: '/sound/ת.mp3' }
 ];
 // Function to play sound associated with a letter
 function playSound(soundPath) {
